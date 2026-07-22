3f409809b25e477f04469b478d1b7e5570a49c52
Sairam Gadde <sairamgadde83@gmail.com>
2026-07-22 20:09:54 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,46 @@
+
+class MinStack {
+    Stack<Integer> stack;
+        Stack<Integer> minstack;
+
+            public MinStack() {
+
+                   stack = new Stack<>();
+                           minstack = new Stack<>();
+                                   
+                                           
+                                               }
+                                                   
+                                                       public void push(int val) {
+
+                                                               stack.push(val);
+
+                                                                       if(minstack.isEmpty()){
+                                                                                   minstack.push(val);
+                                                                                           }
+                                                                                                   else{
+                                                                                                              int currentmin = minstack.peek();
+
+                                                                                                                          minstack.push(Math.min(currentmin,val));
+                                                                                                                                  }
+                                                                                                                                          
+                                                                                                                                              }
+                                                                                                                                                  
+                                                                                                                                                      public void pop() {
+
+                                                                                                                                                             
+                                                                                                                                                                         stack.pop();
+                                                                                                                                                                                     minstack.pop();
+                                                                                                                                                                                             
+                                                                                                                                                                                                     
+                                                                                                                                                                                                         }
+                                                                                                                                                                                                             
+                                                                                                                                                                                                                 public int top() {
+                                                                                                                                                                                                                         return stack.peek();
+                                                                                                                                                                                                                                 
+                                                                                                                                                                                                                                     }
+                                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                             public int getMin() {
+                                                                                                                                                                                                                                                     return minstack.peek();
+                                                                                                                                                                                                                                                         }
+                                                                                                                                                                                                                                                         }
